ae42c5d050e930f649cfe34bb531f1889a735acf
Eydefory <kasimovol2011@mail.ru>
2026-07-02 23:57:42 +0300

    nothing changed..

--- SFMLTemplate/Constants.h
+++ SFMLTemplate/Constants.h
@@ -8,7 +8,6 @@ namespace ApplesGame
 	const float INITIAL_SPEED = 100.f;
 	const float PLAYER_SIZE = 20.f;
 	const float ACCELERATION = 20.f;
-
 	const float APPLE_SIZE = 20.f;
 	const int NUM_ROCKS = 10;
 	const float ROCK_SIZE = 20;
